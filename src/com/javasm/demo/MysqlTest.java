8826b2c869f2c6cd62cb289f151d318fea32e6ed
zhangzhibin123 <2589310695@qq.com>
2022-07-18 20:05:47 +0800

    第5次提交mysqlDemo123

--- src/com/javasm/demo/MysqlTest.java
+++ src/com/javasm/demo/MysqlTest.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class MysqlTest {
     public static void main(String[] args) {
         findStudentByPage();
+        System.out.println("master");
     }
 
     private static void findStudentByPage() {
